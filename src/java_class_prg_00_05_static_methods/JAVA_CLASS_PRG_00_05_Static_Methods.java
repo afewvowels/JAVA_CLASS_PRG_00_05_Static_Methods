7a4f7bc9b6b5079ca1727a122ce34d22757c746c
kbsmith01 <kbsmith01@TS15849.ysu.local>
2018-01-24 12:15:47 -0500

    Initial application created.

---
+++ src/java_class_prg_00_05_static_methods/JAVA_CLASS_PRG_00_05_Static_Methods.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_00_05_static_methods;
+
+/**
+ *
+ * @author kbsmith01
+ */
+public class JAVA_CLASS_PRG_00_05_Static_Methods {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
